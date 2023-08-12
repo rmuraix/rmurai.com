e5770af1cc837b7449df22e4a1b678cefae9f9f0
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-08-12 17:14:38 +0900

    Add works page

---
+++ app/works/[slug]/page.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from "next";
+import { getWorksDetail } from "@/libs/microcms";
+import Article from "@/app/components/article";
+import Link from "next/link";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    dk: string;
+  };
+};
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const data = await getWorksDetail(params.slug, {
+    draftKey: searchParams.dk,
+  });
+
+  return {
+    title: data.title,
+    description: data.summary,
+    openGraph: {
+      title: data.title,
+      description: data.summary,
+    },
+  };
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getWorksDetail(params.slug, {
+    draftKey: searchParams.dk,
+  });
+  return (
+    <>
+      <main className="mx-auto max-w-5xl p-8 sm:p-4">
+        <Article data={data} />
+      </main>
+    </>
+  );
+}
