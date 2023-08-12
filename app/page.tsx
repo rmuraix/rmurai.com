326af706c237bcb77f382d117443efede3f96d96
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-08-12 19:27:49 +0900

    Separate ArrowIcon

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,10 @@ import { languagesFrameworks } from "@/libs/skills";
 import warehouse from "public/images/warehouse.jpg";
 import eno from "public/images/eno.jpg";
 import film from "public/images/film.jpg";
+import ArrowIcon from "./components/icon/arrow";
 
 config.autoAddCss = false;
 
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
 type sociallink = {
   icon: IconProp;
   link: string;
